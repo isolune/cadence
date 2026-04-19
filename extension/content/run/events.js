b4619e301806bd7595a27b3aad6e58e28ddb4a05
lum <katamariontherocks@protonmail.com>
2026-04-19 01:14:19 -0400

    eager/lazy

--- extension/content/run/events.js
+++ extension/content/run/events.js
@@ -1,6 +1,13 @@
 "use strict";
 
+const EventsFlag = Object.freeze({
+    NONE: 0,
+    EAGER: 1,
+    LAZY: 2
+});
+
 const Events = (function() {
+    const ActiveListeners = new WeakSet();
     const Registry = new Map();
 
     let MainController = new AbortController();
@@ -17,48 +24,56 @@ const Events = (function() {
 
     /// Public
 
-    function listen(spec, {
-        managed = true
+    function define(spec, {
+        managed = true,
+        mode = EventsFlag.NONE
     } = {}) {
         const symbol = Symbol(`events.key:${spec.type}`);
 
         Registry.set(symbol, {
             spec,
-            managed
-        });
-
-        add(spec, {
-            managed
-        });
-
-        return symbol;
-    }
-
-    function resume() {
-        if (active()) {
-            return;
-        }
-
-        MainController = new AbortController();
-
-        for (const { spec, managed } of Registry.values()) {
-            if (!managed) {
-                continue;
+            params: {
+                managed,
+                mode
             }
+        });
 
+        if (
+            (Script.active && managed && mode !== EventsFlag.LAZY) ||
+            mode === EventsFlag.EAGER
+        ) {
             add(spec, {
                 managed
             });
         }
+
+        return symbol;
     }
 
-    function suspend() {
-        if (!active()) {
-            return;
-        }
+    function halt(event) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+    }
 
-        MainController.abort();
-        MainController = null;
+    function listen(symbol) {
+        across(symbol, (s) => {
+            const record = Registry.get(s);
+
+            if (record === undefined) {
+                return;
+            }
+
+            const {
+                spec,
+                params: {
+                    managed
+                }
+            } = record;
+
+            add(spec, {
+                managed
+            });
+        });
     }
 
     function unlisten(symbol) {
@@ -73,33 +88,31 @@ const Events = (function() {
                 spec
             } = record;
 
-            Registry.delete(s);
-
             remove(spec);
         });
     }
 
     /// Private
 
-    function active() {
-        return MainController !== null;
-    }
-
-    function add({
-        target = document,
-        type,
-        handler,
-        options
-    }, {
+    function add(spec, {
         managed
     }) {
-        if (!active()) {
-            return;
+        if (ActiveListeners.has(spec)) {
+            throw new UnexpectedError(`Added event twice: '${spec.type}'`);
         }
+
+        const {
+            target = document,
+            type,
+            handler,
+            options
+        } = spec;
 
         const signal = managed
             ? MainController.signal
             : undefined;
+
+        ActiveListeners.add(spec);
 
         across(type, (t) => {
             target.addEventListener(t, handler, {
@@ -109,21 +122,67 @@ const Events = (function() {
         });
     }
 
-    function remove({
-        target = document,
-        type,
-        handler,
-        options
+    function start({
+        primeRun
     }) {
+        MainController ??= new AbortController();
+
+        for (const { spec, params: { managed, mode } } of Registry.values()) {
+            if (
+                !managed ||
+                (primeRun && mode === EventsFlag.EAGER) ||
+                mode === EventsFlag.LAZY
+            ) {
+                continue;
+            }
+
+            add(spec, {
+                managed
+            });
+        }
+    }
+
+    function remove(spec) {
+        if (!ActiveListeners.delete(spec)) {
+            return;
+        }
+
+        const {
+            target = document,
+            type,
+            handler,
+            options
+        } = spec;
+
         across(type, (t) => {
             target.removeEventListener(t, handler, options);
         });
     }
 
+    function stop() {
+        MainController?.abort();
+        MainController = null;
+
+        for (const { spec, params: { managed } } of Registry.values()) {
+            if (!managed) {
+                continue;
+            }
+
+            ActiveListeners.delete(spec);
+        }
+    }
+
+    /// Init
+
+    Script.ifBlocked(stop);
+
+    Script.onActive(start);
+    Script.onSuspended(stop);
+
     return Object.freeze({
+        define,
+        halt,
         listen,
-        resume,
-        suspend,
         unlisten
     });
 })();
